103dd73e697cae38e05250acc6e4117e3cbfc7af
Seong-Jin Kim <sys.ryan0902@gmail.com>
2021-11-27 21:16:57 +0900

    feat: Writing Custom Components

---
+++ 01-starting-setup/src/Components/ExpenseItem.js
@@ -0,0 +1,11 @@
+function ExpenseItem() {
+  return (
+    <div>
+      <div>March 28th 2021</div>
+      <h2>Car Insurance</h2>
+      <div>$300</div>
+    </div>
+  );
+}
+
+export default ExpenseItem;
