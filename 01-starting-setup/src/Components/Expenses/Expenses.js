9e6b1e95c04f650fb7d5f1b4edecf1872415f47e
Seong-Jin Kim <sys.ryan0902@gmail.com>
2021-11-28 22:09:00 +0900

    feat: working with events and states
    - add year filter

--- 01-starting-setup/src/Components/Expenses/Expenses.js
+++ 01-starting-setup/src/Components/Expenses/Expenses.js
@@ -1,13 +1,26 @@
-import ExpenseItem from "./ExpenseItem";
+import React, { useState } from "react";
+
 import Card from "./UI/Card";
+
+import ExpenseItem from "./ExpenseItem";
+import ExpenseFilter from "./ExpenseFilter";
 
 import "./Expenses.css";
 
 const Expenses = (props) => {
+  const [filteredYear, setFilteredYear] = useState("2021");
   const expenses = props.expenses;
+
+  const filterChangeHandler = (selectedYear) => {
+    setFilteredYear(selectedYear);
+  };
 
   return (
     <Card className="expenses">
+      <ExpenseFilter
+        selected={filteredYear}
+        onFilterChange={filterChangeHandler}
+      />
       <ExpenseItem
         title={expenses[0].title}
         amount={expenses[0].amount}
