2049b460596a6423506fa612fdeeb531688abe18
Seong-Jin Kim <sys.ryan0902@gmail.com>
2021-11-29 01:40:24 +0900

    fix: amount type error
    - string to number

--- 01-starting-setup/src/Components/NewExpese/ExpenseForm.js
+++ 01-starting-setup/src/Components/NewExpese/ExpenseForm.js
@@ -53,7 +53,7 @@ const ExpenseForm = (props) => {
 
     const expenseData = {
       title: userInput.enteredTitle,
-      amount: userInput.enteredAmount,
+      amount: +userInput.enteredAmount,
       date: new Date(userInput.enteredDate),
     };
 
