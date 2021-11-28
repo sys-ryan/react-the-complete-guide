49fdfc2e2d1b382569f0d6ea2ebb6c61c0943773
Seong-Jin Kim <sys.ryan0902@gmail.com>
2021-11-28 18:16:04 +0900

    feat: adding form inputs

--- 01-starting-setup/src/App.js
+++ 01-starting-setup/src/App.js
@@ -1,6 +1,7 @@
 import React from "react";
 
 import Expenses from "./Components/Expenses/Expenses";
+import NewExpense from "./Components/NewExpese/NewExpense";
 
 const App = () => {
   const expenses = [
@@ -34,7 +35,7 @@ const App = () => {
 
   return (
     <div>
-      <h2>Lets get started!</h2>
+      <NewExpense />
       <Expenses expenses={expenses} />
     </div>
   );
