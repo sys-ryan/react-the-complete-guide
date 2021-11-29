c992837273bf0bf893e85910f1bebf338625fcd3
Seong-Jin Kim <sys.ryan0902@gmail.com>
2021-11-30 00:35:15 +0900

    feat: 05 create AddUesr Component

---
+++ 05-Practice_Project/src/components/Users/AddUser.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+const AddUser = (props) => {
+  const addUserHandler = (event) => {
+    event.preventDefault();
+  };
+  return (
+    <form onSubmit={addUserHandler}>
+      <label htmlFor="username">UserName</label>
+      <input type="text" id="username" />
+      <label htmlFor="age">Age (Years)</label>
+      <input type="number" id="age" />
+      <button type="submit">Add User</button>
+    </form>
+  );
+};
+
+export default AddUser;
